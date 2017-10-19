1dcaa15a6e4fdbf3f3c2439c3f467475d2c7e3e5
Ezequiel O. Fogliatto <ezefogliatto@gmail.com>
2017-10-19 11:36:34 -0300

    Selecci\'on del lenguaje. Faltan instalar locales

--- scrabble.py
+++ scrabble.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
+import locale
+
 
 
 
 def diagrama( lan = 'es' ):
 
+    
     """
     Creaci\'on del diagrama con las fichas de scrabble
     """
@@ -16,26 +19,42 @@ def diagrama( lan = 'es' ):
     # Seleccion de fichas de acuerdo al idioma
 
     fichas = {}
+
     
     if lan == 'es':
 
         fichas = fch.fichas_es
+        
+        locale.setlocale(locale.LC_ALL, ('en_US', 'UTF-8'))
 
+        
     elif lan == 'en' :
 
         fichas = fch.fichas_en
+
+        locale.setlocale(locale.LC_ALL, ('en_US', 'UTF-8'))
+                
 
     elif lan == 'it' :
 
         fichas = fch.fichas_it
 
+        locale.setlocale(locale.LC_ALL, ('en_US', 'UTF-8'))
+
+
     elif lan == 'fr' :
 
         fichas = fch.fichas_fr
 
+        locale.setlocale(locale.LC_ALL, ('en_US', 'UTF-8'))
+        
+
     elif lan == 'ar' :
 
-        fichas = fch.fichas_ar        
+        fichas = fch.fichas_ar
+
+        locale.setlocale(locale.LC_ALL, ('en_US', 'UTF-8'))
+                
         
 
 
@@ -58,8 +77,6 @@ def diagrama( lan = 'es' ):
     # Creacion de la figura inicial
 
     fix, ax = plt.subplots()
-
-    # ax.axis( color = 'silver' )
 
     ax.axis( 'off' )
 
@@ -93,11 +110,14 @@ def diagrama( lan = 'es' ):
 
     idy = 0
 
-    for key in sorted(fichas):
+    
+    # for k in sorted( fichas.keys(), key=lambda word: [alphabet.index(c) for c in word[0]] ):
 
-        if key != 'blank' :
+    for k in sorted( fichas.keys() ):
 
-            for i in range( fichas[key][0] ):
+        if k != 'blank' :
+
+            for i in range( fichas[k][0] ):
 
                 idx = idx + 1
 
@@ -110,20 +130,14 @@ def diagrama( lan = 'es' ):
 
                 # Posicion de la ficha
                 
-                ax.text( 0.5 + idx, 9.5 - idy, key.upper(), ha="center", va="center", size = 15, fontweight = 'bold' )
+                ax.text( 0.5 + idx, 9.5 - idy, k.upper(), ha="center", va="center", size = 15, fontweight = 'bold' )
 
                 
                 # Valor de la ficha
                 
-                ax.text( 0.8 + idx, 9.2 - idy, fichas[key][1], ha="center", va="center", size = 5, fontweight = 'bold' )
+                ax.text( 0.8 + idx, 9.2 - idy, fichas[k][1], ha="center", va="center", size = 5, fontweight = 'bold' )
                 
                 
-
-
-        # print(key)
-
-
-
 
 
     
